bab598a9a5c803aae6a13a535a6d9e7d824bb728
mrshaneez <mr.shaneez@gmail.com>
2026-08-27 14:29:44 +0500

    Delete MyCases-fixed/MyCases directory

--- MyCases-fixed/MyCases/vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-// Relative base so the build works on GitHub Pages under /<repo>/
-// as well as from a local file or any other host.
-export default defineConfig({
-  base: "./",
-  plugins: [react()],
-});
